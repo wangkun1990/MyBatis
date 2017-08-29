f3fa9dbc0ce20e2959df1aaea9d8d42356f31ad2
wangkun <kennywhite@163.com>
2017-08-29 08:58:32 +0800

    add and update some files

---
+++ src/main/java/com/learn/service/impl/StudentUtils.java
@@ -0,0 +1,21 @@
+package com.learn.service.impl;
+
+import com.learn.dao.StudentMapper;
+import com.learn.entity.Student;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+
+@Component
+public class StudentUtils {
+    private static StudentMapper staticStudentMapper;
+
+    @Resource
+    public void setStudentMapper(StudentMapper studentMapper) {
+        StudentUtils.staticStudentMapper = studentMapper;
+    }
+
+    public static Student getStudent(int id) {
+        return staticStudentMapper.getStudentById(id);
+    }
+}
