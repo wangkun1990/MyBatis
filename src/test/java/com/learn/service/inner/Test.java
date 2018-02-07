8f069bed8a8d79a405784ae0f9e61dc1e555283d
wangkun <kennywhite@163.com>
2018-02-07 14:54:23 +0800

    修改内容

--- src/test/java/com/learn/service/inner/Test.java
+++ src/test/java/com/learn/service/inner/Test.java
@@ -4,22 +4,6 @@ import java.io.UnsupportedEncodingException;
 
 public class Test {
 
-    /*
-     * 判断输入的字符串在某个字符集下是否有效
-     */
-    public static boolean isStringValidInCharset(String s, String charset) {
-        try {
-            // 获取字符串s在字符集charset下的编码
-            byte[] bytes = s.getBytes(charset);
-            // 把获得的编码按该字符集解码成新字符串
-            String ss = new String(bytes, charset);
-            // 解码出来的新字符串应该与原来相等
-            // 如果s不是该字符集下有效的字符串，解码出来的会是一堆问号
-            return s.equals(ss);
-        } catch (UnsupportedEncodingException e) {
-            return false;
-        }
-    }
 
     public static void main(String[] args) {
 
@@ -27,12 +11,6 @@ public class Test {
         String b = "䣃";
 
         try {
-            //System.out.println (System.getenv("LC_CTYPE"));
-
-            System.out.println (isStringValidInCharset(a, "GBK"));
-            System.out.println (isStringValidInCharset(b, "GBK"));
-            System.out.println (isStringValidInCharset(b, "CP936"));
-
             byte[] aa = a.getBytes("GBK");
             System.out.println (javax.xml.bind.DatatypeConverter.printHexBinary(aa));
             System.out.println (new String(aa, "GBK"));
--- src/test/java/com/learn/service/inner/Test2.java
+++ src/test/java/com/learn/service/inner/Test2.java
@@ -1,6 +1,9 @@
 package com.learn.service.inner;
 
 
+import org.apache.commons.lang3.StringUtils;
+
+import java.io.UnsupportedEncodingException;
 
 public class Test2 {
 
@@ -53,14 +56,56 @@ public class Test2 {
         System.out.println(new String(iso88591Msg, "iso-8859-1"));
 
 
+        System.out.println("--------------------------------");
+        System.out.println(ensureNoMessy("椪", 20));
+
+        System.out.println(ensureNoMessy("好", 20));
+
+        System.out.println(ensureNoMessy("㵪", 20));
+
+        System.out.println(ensureNoMessy("123你好", 20));
 
 
+        System.out.println(javax.xml.bind.DatatypeConverter.printHexBinary(String.valueOf('椪').getBytes("GB2312")));
+        System.out.println(javax.xml.bind.DatatypeConverter.printHexBinary(String.valueOf('椪').getBytes("GBK")));
 
 
+        System.out.println(javax.xml.bind.DatatypeConverter.printHexBinary(String.valueOf('㵪').getBytes("GB2312")));
+        System.out.println(javax.xml.bind.DatatypeConverter.printHexBinary(String.valueOf('㵪').getBytes("GBK")));
+    }
 
 
-
-
-
+    public static String ensureNoMessy(String str, int length) {
+        // 字符串不为空，且长度小于定义的长度则无需截取字符创
+        if (StringUtils.isNotEmpty(str)) {
+            int bytes = 0; // 用来存储字符串的总字节数
+            try {
+                int GB2312BytesLength = str.getBytes("GB2312").length;
+                System.out.println("GB2312BytesLength = " + GB2312BytesLength);
+                int GBKBytesLength = str.getBytes("GBK").length;
+                System.out.println("GBKBytesLength = " + GBKBytesLength);
+                if (length > GB2312BytesLength) {
+                    //若实际长度小于表定义的长度 则将传入的表定义长度重新赋值.
+                    length = GB2312BytesLength;
+                }
+            } catch (UnsupportedEncodingException e) {
+                // Ignore
+            }
+            for (int i = 0; i < str.length(); i++) {
+                if (bytes == length) {
+                    return str.substring(0, i);
+                }
+                char c = str.charAt(i);
+                if (c < 256) {
+                    bytes += 1; // 英文字符的字节数看作1
+                } else {
+                    bytes += 2; // 中文字符的字节数看作2
+                    if (bytes - length == 1) {
+                        return str.substring(0, i);
+                    }
+                }
+            }
+        }
+        return str;
     }
 }
