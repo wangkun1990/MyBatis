b178ba1d63a26cec7595e7fa3af70a2d1ff27f9d
wangkun <kun.wang@yoho.cn>
2018-07-04 15:12:31 +0800

    修改

--- src/main/java/com/learn/dao/DepartmentMapper.java
+++ src/main/java/com/learn/dao/DepartmentMapper.java
@@ -2,8 +2,7 @@ package com.learn.dao;
 
 import com.learn.entity.Department;
 import com.learn.entity.Department2;
-import com.learn.mybatis.tool.BaseMapper;
-import com.learn.mybatis.tool.BaseSelectMapper;
+import com.learn.mybatis.mapper.BaseMapper;
 
 /**
  * Created by wangkun on 2017/4/16.
@@ -12,10 +11,10 @@ public interface DepartmentMapper extends BaseMapper<Department> {
 
     Department getDepartmentById(int id);
 
-
     Department2 getDepartment2ById(int id);
 
     /**
+     * 插入对象
      * @param department
      * @return
      */
