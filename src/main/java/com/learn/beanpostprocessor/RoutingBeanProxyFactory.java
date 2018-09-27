23f3aa530b756b08f0a566e5b061eedaded56ea4
wangkun <kun.wang@yoho.cn>
2018-09-27 18:04:09 +0800

    修改方法

--- src/main/java/com/learn/beanpostprocessor/RoutingBeanProxyFactory.java
+++ src/main/java/com/learn/beanpostprocessor/RoutingBeanProxyFactory.java
@@ -2,17 +2,16 @@ package com.learn.beanpostprocessor;
 
 import com.learn.beanpostprocessor.annotation.RoutingSwitch;
 import com.learn.beanpostprocessor.routingenum.RoutingEnum;
-import com.learn.beanpostprocessor.util.RoutingVersion;
 import org.aopalliance.intercept.MethodInterceptor;
 import org.aopalliance.intercept.MethodInvocation;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.aop.framework.ProxyFactory;
-import org.springframework.cglib.proxy.MethodProxy;
 
 import java.lang.reflect.Method;
 import java.util.Map;
 
 public class RoutingBeanProxyFactory {
+
     public static Object createProxy(Class targetClass, Map<String, Object> beans) {
         ProxyFactory proxyFactory = new ProxyFactory();
         proxyFactory.setInterfaces(targetClass);
@@ -22,17 +21,12 @@ public class RoutingBeanProxyFactory {
 
     static class VersionRoutingMethodInterceptor implements MethodInterceptor {
 
-        //private String classSwitch;
-
         private Object beanOfVersionA;
 
         private Object beanOfVersionB;
 
         public VersionRoutingMethodInterceptor(Class targetClass, Map<String, Object> beans) {
             String interfaceName = StringUtils.uncapitalize(targetClass.getSimpleName());
-            /*if (targetClass.isAnnotationPresent(RoutingSwitch.class)) {
-                this.classSwitch = ((RoutingSwitch) targetClass.getAnnotation(RoutingSwitch.class)).value();
-            }*/
             this.beanOfVersionA = beans.get(this.buildBeanName(interfaceName, true));
             this.beanOfVersionB = beans.get(this.buildBeanName(interfaceName, false));
         }
@@ -49,10 +43,6 @@ public class RoutingBeanProxyFactory {
             if (method.isAnnotationPresent(RoutingSwitch.class)) {
                 switchName = method.getAnnotation(RoutingSwitch.class).value().name();
             }
-            /*if (StringUtils.isBlank(switchName)) {
-                throw new IllegalStateException("RoutingSwitch value is blank, method:" + method.getName());
-            }*/
-
             return invocation.getMethod().invoke(getTargetBean(switchName), invocation.getArguments());
         }
 
@@ -62,8 +52,6 @@ public class RoutingBeanProxyFactory {
                 switchOn = true;
             } else if (RoutingEnum.B.name().equals(switchName)) {
                 switchOn = false;
-            } else {
-                //switchOn = FunctionSwitch.isSwitchOpened(switchName);
             }
             return switchOn ? beanOfVersionA : beanOfVersionB;
         }
